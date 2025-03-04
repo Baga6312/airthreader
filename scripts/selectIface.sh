5f7a73782b24c039d9308c306f8ca5ae61003172
Baga6312 <magroubertn@gmail.com>
2025-03-04 04:53:32 +0100

    commited at Tue Mar  4 04:53:32 AM CET 2025 from rabbit99

--- scripts/selectIface.sh
+++ scripts/selectIface.sh
@@ -2,23 +2,22 @@
 
 printf 'Select the Interface to perform the Attack:\n'
 
-# Get interfaces (improved column parsing)
+# get interfaces (improved column parsing)
 mapfile -t interfaces < <(ip -br link | grep -v -e docker0 -e lo | awk '{print $1}')
 
-# List interfaces with numbers
 for i in "${!interfaces[@]}"; do  # Use array indices
     printf '%d:\t%s\n' "$i"    "${interfaces[i]}"
 done
 
-# Get user selection
+# get user selection
 read -p "Enter interface number: " selection
 
-# Validate input
+# validate input
 if [[ "$selection" =~ ^[0-${!interfaces[@]}]+$ ]] && [ "$selection" -lt "${#interfaces[@]}" ]; then
     selected_iface="${interfaces[selection]}"
     echo "Selected interface: $selected_iface"
     ./changeIfaceMode.sh $selected_iface 
-    # Add your attack logic here
+# add your attack logic here
 else
     echo "Invalid selection!" >&2
     exit 1
